d0ba69e2fe994929020adb4bb28da7d5d3865d53
youbinoyou <youbinoyou@gmail.com>
2019-06-07 16:47:38 +0900

    Books 本紹介ページ

---
+++ Books/js/db.js
@@ -0,0 +1,39 @@
+var db = [];
+    var books_data = [
+        {
+            title:"大人の人間関係",
+            price:"850",
+            bookid:"C2077",
+            book_image_url:"https://images-na.ssl-images-amazon.com/images/I/51Te41dLF8L._SX339_BO1,204,203,200_.jpg",
+            book_amazon:"https://www.amazon.co.jp/%E5%A4%A7%E4%BA%BA%E3%81%AE%E4%BA%BA%E9%96%93%E9%96%A2%E4%BF%82-%E7%9F%B3%E5%8E%9F-%E5%A3%AE%E4%B8%80%E9%83%8E/dp/4537215518",
+            items : [
+                {
+                    item_title:"プロローグ",
+                    title:"大人の人間関係をラクに生き抜く",
+                },
+                {
+                    item_title:"第一章",
+                    title:"ビジネスの人間関係（上司、部下、同僚）",
+                },
+                {
+                    item_title:"第二章",
+                    title:"家族の人間関係（親、夫婦、きょうだい）",
+                },
+                {
+                    item_title:"第三章",
+                    title:"知人の人間関係（ママ友、友人、隣人）",
+                },
+                {
+                    item_title:"第四章",
+                    title:"恋愛の人間関係（恋人、異性の友人）",
+                },
+                {
+                    item_title:"第五章",
+                    title:"ストレスを溜めない処世術 大人力テスト",
+                },
+            ]
+        },
+    ];
+    function db() {
+    
+    }
